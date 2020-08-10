da6781a7d9de95ca73e8b4751028444fcdf6150d
Tom Taylor <hello@tomtaylor.name>
2020-08-10 18:30:38 +0100

    Add basic unit test

--- tests/module.spec.js
+++
@@ -1,5 +0,0 @@
-// const {App} = require('adapt-authoring-core');
-
-// testing fails at the moment because of error if test is started from the main projects npm script ->
-//  UnhandledPromiseRejectionWarning: Error: Cannot find module 'adapt-authoring-core'
-
