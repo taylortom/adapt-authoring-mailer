f44680139d387887f8ae79a2c496ee5a331f3f9b
Ailiniyazi <a.maimaiti@fkc-online.com>
2020-08-14 10:30:03 +0200

    refactored mail send function

--- tests/mailerModule.spec.js
+++ tests/mailerModule.spec.js
@@ -2,24 +2,30 @@ const nodemailer = require("nodemailer");
 const { App } = require('adapt-authoring-core');
 
 describe('Mailer module', function() {
-  before(async function() {
-    this.mailer = await App.instance.waitForModule('mailer');
+    before(async function() {
+        this.mailer = await App.instance.waitForModule('mailer');
 
-    const { user, pass, smtp } = await nodemailer.createTestAccount();
+        const { user, pass, smtp } = await nodemailer.createTestAccount();
 
-    App.instance.config.set(`${this.mailer.name}.enable`, true);
-    App.instance.config.set(`${this.mailer.name}.useConnectionUrl`, true);
-    App.instance.config.set(`${this.mailer.name}.connectionUrl`, `smtp://${user}:${pass}@${smtp.host}:${smtp.port}`);
+        App.instance.config.set(`${this.mailer.name}.enable`, true);
+        App.instance.config.set(`${this.mailer.name}.useConnectionUrl`, true);
+        App.instance.config.set(`${this.mailer.name}.connectionUrl`, `smtp://${user}:${pass}@${smtp.host}:${smtp.port}`);
 
-    await this.mailer.init();
-  });
-  describe('#initialise()', function() {
-    it('should be able to test valid SMTP credentials', async function() {
-      await this.mailer.testConnection();
+        await this.mailer.init();
     });
-    it('should be able to send an email', async function() {
-      const data = await this.mailer.send('test@mail.com', 'Hello', "world");
-      data.accepted.length.should.equal(1);
+    describe('#initialise()', function() {
+        it('should be able to test valid SMTP credentials', async function() {
+            await this.mailer.testConnection();
+        });
+        it('should be able to send an email', async function() {
+            var message = {
+                to: "test@mail.com",
+                subject: "Hello",
+                text: "Plaintext version of the message",
+                html: "<p>HTML version of the message</p>"
+            };
+            const data = await this.mailer.send(message);
+            data.accepted.length.should.equal(1);
+        });
     });
-  });
 });
