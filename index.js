347047fe71489ce879bbd153107229bdaaeef104
Tom Taylor <hello@tomtaylor.name>
2024-05-15 12:19:31 +0100

    Add export

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
  * Email sending utilities
  * @namespace mailer
  */
+export { default as AbstractMailTransport } from './lib/AbstractMailTransport.js'
 export { default } from './lib/MailerModule.js'
